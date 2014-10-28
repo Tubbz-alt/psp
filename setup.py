8c6e6fb5d39e4db6e1c2d45d2a1bb116694f3dc2
Michael C Browne <mcbrowne@slac.stanford.edu>
2014-10-28 21:59:39 +0000

    Restructure for installation.

---
+++ setup.py
@@ -0,0 +1,6 @@
+from distutils.core import setup
+
+setup(name='psp', version='1.0.0', packages=['psp'],
+      description='pyca utilities',
+      author='Amedeo Perazzo', author_email='amedeo@slac.stanford.edu')
+
