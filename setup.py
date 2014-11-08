85a017b22fd9dc38f08e95f369300e1f8432a130
Daniel Flath <dflath@slac.stanford.edu>
2014-11-08 03:34:52 +0000

    Bump version to 1.1.0 in anticipation of new tag.  Requires pyca R2.1.0

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from distutils.core import setup
 
-setup(name='psp', version='1.0.0', packages=['psp'],
+setup(name='psp', version='1.1.0', packages=['psp'],
       description='pyca utilities',
       author='Amedeo Perazzo', author_email='amedeo@slac.stanford.edu')
-
