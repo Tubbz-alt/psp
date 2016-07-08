a2d0d64168e0207807de59acaa87b93c951bb9f6
Michael C Browne <mcbrowne@slac.stanford.edu>
2016-07-08 22:30:55 +0000

    Bump for pyca 2.1.1.

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from distutils.core import setup
 
-setup(name='psp', version='2.0.0', packages=['psp'],
+setup(name='psp', version='2.0.1', packages=['psp'],
       description='pyca utilities',
       author='Amedeo Perazzo', author_email='amedeo@slac.stanford.edu')
