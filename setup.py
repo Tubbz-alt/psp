60b576f0b69c0d2f956cfa095a09c13dda5d9b0f
Zachary Lentz <zlentz@slac.stanford.edu>
2016-08-13 00:20:48 +0000

    bump version number

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from distutils.core import setup
 
-setup(name='psp', version='2.0.2', packages=['psp'],
+setup(name='psp', version='2.0.3', packages=['psp'],
       description='pyca utilities',
       author='Amedeo Perazzo', author_email='amedeo@slac.stanford.edu')
